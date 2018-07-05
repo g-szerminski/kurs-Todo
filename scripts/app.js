b27017b1599ab21bac8d0329d97ae35aaead9c4a
Grzegorz Szermiński <szerman@poczta.fm>
2018-07-05 22:04:29 +0200

    Exercise In Progress

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,9 @@ var todolist = {
 	todos: ['item 1', 'item 2', 'item 3'],
 	displayTodos: function() {
 		console.log('My todos: ', this.todos);
+	},
+	addTodo: function(todo){
+		this.todos.push(todo);
+		this.displayTodos();
 	}
 };
