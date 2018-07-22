dde9f0e4e3b57518ee8f0147838b46bb60acf62d
Grzegorz Szermiński <szerman@poczta.fm>
2018-07-22 10:33:04 +0200

    Work In Progress

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,16 @@ var todoList = {
     displayTodos: function () {
         console.log('My todos: ', this.todos);
     },
-    addTodo: function (todo) {
+    addTodo: function(todo) {
         this.todos.push(todo);
         this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+    	this.todos[position] = newValue;
+    	this.displayTodos();
+    },
+    deleteTodo: function(position) {
+    	this.todos.splice(position, 1);
+    	this.displayTodos();
     }
 }
