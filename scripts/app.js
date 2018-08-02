2483b4786fdc0bec175273cf2c641aea3d4db15a
Grzegorz Szermiński <szerman@poczta.fm>
2018-08-02 11:02:00 +0200

    Add toggle method in to todolist object

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,31 @@
 var todoList = {
-    todos: ['item 1', 'item 2', 'item 3'],
+    todos: [],
     displayTodos: function () {
         console.log('My todos: ', this.todos);
     },
-    addTodo: function(todo) {
-        this.todos.push(todo);
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            complited: false
+        });
         this.displayTodos();
     },
-    changeTodo: function(position, newValue) {
-    	this.todos[position] = newValue;
-    	this.displayTodos();
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
     },
     deleteTodo: function(position) {
     	this.todos.splice(position, 1);
     	this.displayTodos();
+    },
+    toggleComplited: function(position) {
+        var todo = this.todos[position];
+        todo.complited = !todo.complited;
+        this.displayTodos();
     }
 }
+
+ // changeTodo: function(position, newValue) {
+    //  this.todos[position] = newValue;
+    //  this.displayTodos();
+    // },
