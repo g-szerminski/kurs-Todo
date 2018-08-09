4fbf6a0690fcca1a82641cb26bd0573d742ae142
Grzegorz Szermiński <szerman@poczta.fm>
2018-08-09 17:51:27 +0200

    Add exercies.js file with js exercises

---
+++ scripts/exercises.js
@@ -0,0 +1,24 @@
+// zad.1
+var arr = [1, 1, 2, 3, 5, 8, 13, 21];
+var arr2 = [2, 3, 6, 8, 12 ,15 ,23 ,63 ,1, 100];
+function myArrayMax(array) {
+    return Math.max.apply(null, array);
+}
+
+//zad.2
+function max(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+//zad.3
+function sum() {
+
+}
+
+function multiply() {
+
+}
