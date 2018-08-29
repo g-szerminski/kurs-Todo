0c11b3a3a8e8e6ba3a1d4d10d4d0d66e3c80bdc3
Grzegorz Szermiński <szerman@poczta.fm>
2018-08-29 20:37:59 +0200

    Some changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 var todoList = {
     todos: [],
     displayTodos: function () {
-        debugger;
         if (this.todos.length === 0) {
             console.log('Your todo list is empty !');
         } else {
